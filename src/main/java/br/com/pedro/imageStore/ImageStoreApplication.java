f6922d0415e4c251b3da9c223edba515aa338e94
Pedro Albuquerque <joao.albuquerque@gruporas.com.br>
2025-12-05 17:45:45 -0300

    initial commit, initial project

---
+++ src/main/java/br/com/pedro/imageStore/ImageStoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.pedro.imageStore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageStoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pedro/imageStore/ImageStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pedro.imageStore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImageStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
